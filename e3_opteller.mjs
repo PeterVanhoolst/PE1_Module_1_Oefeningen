a4e892a2fb1eb0f43a9327884bfee23432a7c08e
PeterVanhoolst <peter.vanhoolst@gmail.com>
2023-09-25 13:42:59 +0200

    Update e3_opteller.mjs
    automatische opteller van twee getallen

--- e3_opteller.mjs
+++ e3_opteller.mjs
@@ -4,8 +4,10 @@ import{stdin as input, stdout as output} from 'node:process';
 const userInput = readline.createInterface({input, output});
 // Einde van userInput voorbereiden
 
+let getal1 = parseFloat(await userInput.question("Wat is uw eerste getal?"));
+let getal2 = parseFloat(await userInput.question("Wat is uw tweede getal?"));
 
-
+console.log(getal1+getal2);
 // Voeg hier je eigen code in
 
 
