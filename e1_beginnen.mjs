f65cdfe921506bc1118fdd741bf0db31c4a630a7
PeterVanhoolst <peter.vanhoolst@gmail.com>
2023-09-25 13:22:57 +0200

    Update e1_beginnen.mjs
    code geschreven voor de statische weergave van info

--- e1_beginnen.mjs
+++ e1_beginnen.mjs
@@ -1,5 +1,7 @@
 // Voeg hier je eigen code in
-
+let name = ("Peter");
+let age = ("35");
+let favoriteGame = ("Final Fantasy");
 
 
 console.log('Hallo '+ name +', ik hoor dat je '+ age +' jaar oud bent en dat je lievelingsgame '+ favoriteGame +' is.');
